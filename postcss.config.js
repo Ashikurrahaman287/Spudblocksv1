5be4906c1ab46581c0820c458e748ca11c38061b
Ashikurrahaman287 <nishuthetomlover@gmail.com>
2025-11-07 20:16:38 +0600

    v1

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
-export default {
+// Use CommonJS export to ensure PostCSS loaders (some running under CJS) load
+// the plugin list correctly during build on different environments (Vercel).
+// This avoids issues where the config is read as an ESM module and tooling
+// doesn't receive the plugins object as expected.
+module.exports = () => ({
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+});
+
