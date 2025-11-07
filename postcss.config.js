71ba223b310ce18582a14ff1bb14bf71c24ecc4f
Ashikurrahaman287 <nishuthetomlover@gmail.com>
2025-11-07 20:56:07 +0600

    fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
-// Provide an ESM-safe export as well so builds that load ESM configs don't
-// error. The canonical CommonJS config lives in `postcss.config.cjs` and
-// will be preferred by tooling that loads CJS; this file avoids runtime
-// errors in ESM contexts.
-export default () => ({
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-});
+    'tailwindcss': {},
+    'autoprefixer': {
+      from: undefined
+    }
+  }
+};
 
